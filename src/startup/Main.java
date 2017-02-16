8009c344a185092ffd2e7739d028618615d8a413
~dreeki~ <~dreeki~@DESKTOP-G68EGDU>
2017-02-16 10:50:19 +0100

    Projecten 2 - Java - Aangemaakt

--- src/startup/Main.java
+++ src/startup/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package project.pkg2.administrator;
+package startup;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -17,7 +17,7 @@ import javafx.stage.Stage;
  *
  * @author ~dreeki~
  */
-public class Project2Administrator extends Application {
+public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
