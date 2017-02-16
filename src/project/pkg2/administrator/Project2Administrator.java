be4ca470f7cb808b7269ff5fe843505169056030
wimde <wimde@DESKTOP-RMKG03Q>
2017-02-16 10:50:54 +0100

    Modified hello world message

--- src/project/pkg2/administrator/Project2Administrator.java
+++ src/project/pkg2/administrator/Project2Administrator.java
@@ -27,7 +27,7 @@ public class Project2Administrator extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hello World! With a test sentence!");
             }
         });
         
